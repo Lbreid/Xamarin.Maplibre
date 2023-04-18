8e1332b8ed376b4320505b86959322f4541311cc
Home <Home@DESKTOP-GDKRON6>
2023-04-19 01:37:48 +1200

    Add project files.

---
+++ Samples/AndroidSample/MainActivity.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Snackbar;
+using Mapbox.Mapboxsdk.Maps;
+
+
+namespace AndroidSample
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
+    {
+
+        MapView mapView;
+        string mapTilerKey = "*****API KEY*****"; 
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            base.OnCreate(savedInstanceState);
+
+            Mapbox.Mapboxsdk.Mapbox.GetInstance(this);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            mapView = FindViewById<MapView>(Resource.Id.mapView);
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+
+            mapView.OnCreate(savedInstanceState);
+            mapView.GetMapAsync(this);
+
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View)sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (View.IOnClickListener)null).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void OnMapReady(MapboxMap map)
+        {
+            string styleUrl = $"https://api.maptiler.com/maps/streets-v2/style.json?key={mapTilerKey}";
+            // Set the style after mapView was loaded
+            map.SetStyle(styleUrl);
+            map.UiSettings.SetAttributionMargins(15, 0, 0, 15);
+        }
+    }
+}
